2a2024411408f88bd348df08761423d843dcc537
Forc3ment <kperigault@gmail.com>
2017-11-06 11:34:46 +0100

    Correct some bug

--- main.cpp
+++ main.cpp
@@ -62,77 +62,83 @@ void createWhiteImage(int u, int v, Mat & white){
 }
 
 //=======================================================================================
-// Sketching
+// Sketching Lines
 //=======================================================================================
 
-void sketchingGray(const Mat & grayImg)
+// void sketchingGray(const Mat & grayImg)
+// {
+//   	Mat gradX, gradY;
+//   	Mat white, overlay;
+
+// 	Sobel(grayImg, gradX, -1, 0, 1);
+//     Sobel(grayImg, gradY, -1, 1, 0);
+
+//     createWhiteImage(grayImg.rows,grayImg.cols,white);
+
+// 	white.copyTo(overlay);
+
+//     int count = 20000;
+//     double alpha = 0.5;
+//     int length = 5;
+//     for (int i = 0; i < count; ++i)
+//     {
+//     	int u = rand() % grayImg.rows;
+//     	int v = rand() % grayImg.cols;
+
+//     	int valGradX = gradX.at<unsigned char>(u,v);
+//     	int valGradY = gradY.at<unsigned char>(u,v);
+
+//     	float norm = sqrt(valGradY*valGradY + valGradX* valGradX);
+
+//     	//cout << norm << endl;
+
+//     	if(norm > 40)
+//     	{
+//     		white.copyTo(overlay);
+// 			line(overlay, Point(v-((valGradX/norm)*length),u-((-valGradY/norm)*length)), Point(v+((valGradX/norm)*length),u+((-valGradY/norm)*length)), Scalar(0,0,0), 1);
+// 			addWeighted(overlay, alpha, white, 1 - alpha, 0, white);
+// 		}
+// 		else
+// 		{
+// 			i--;
+// 		}
+//     }
+
+// 	imshow("white", white);
+// 	imshow("gradX", gradX);
+// 	imshow("gradY", gradY);
+// 	//imshow("gradY", norm_0_255(gradY));
+// 	cvWaitKey();
+// }
+
+void sketchingLines(const Mat & img)
 {
-  	Mat gradX, gradY;
-  	Mat white, overlay;
-
-	Sobel(grayImg, gradX, -1, 0, 1);
-    Sobel(grayImg, gradY, -1, 1, 0);
-
-    createWhiteImage(grayImg.rows,grayImg.cols,white);
-
-	white.copyTo(overlay);
-
-    int count = 20000;
-    double alpha = 0.5;
-    int length = 5;
-    for (int i = 0; i < count; ++i)
-    {
-    	int u = rand() % grayImg.rows;
-    	int v = rand() % grayImg.cols;
-
-    	int valGradX = gradX.at<unsigned char>(u,v);
-    	int valGradY = gradY.at<unsigned char>(u,v);
-
-    	float norm = sqrt(valGradY*valGradY + valGradX* valGradX);
-
-    	//cout << norm << endl;
-
-    	if(norm > 40)
-    	{
-    		white.copyTo(overlay);
-			line(overlay, Point(v-((valGradX/norm)*length),u-((-valGradY/norm)*length)), Point(v+((valGradX/norm)*length),u+((-valGradY/norm)*length)), Scalar(0,0,0), 1);
-			addWeighted(overlay, alpha, white, 1 - alpha, 0, white);
-		}
-		else
-		{
-			i--;
-		}
-    }
-
-	imshow("white", white);
-	imshow("gradX", gradX);
-	imshow("gradY", gradY);
-	//imshow("gradY", norm_0_255(gradY));
-	cvWaitKey();
-}
-
-void sketching(const Mat & img)
-{
-  	Mat gradX, gradY;
-  	Mat gray;
+	cout << "------ Sketching Lines ------" << endl;
+	Mat gradX, gradY;
+  	Mat gray, gray16;
   	Mat white, overlay;
 
   	toGray(img,gray);
-	Sobel(gray, gradX, -1, 0, 1);
-    Sobel(gray, gradY, -1, 1, 0);
+
+  	Sobel(gray, gradX, CV_16S, 0, 1, 3);
+    Sobel(gray, gradY, CV_16S, 1, 0, 3);
+
+    gradX = abs(gradX);
+    gradY = abs(gradY);
+
+    gradX.convertTo(gradX, CV_8U);
+    gradY.convertTo(gradY, CV_8U);
 
     createWhiteImage(img.rows,img.cols,white);
 
-	//Mat visited = zeros(img.rows,img.cols,CV_8U);
-	Mat visited = Mat(img.rows,img.cols, CV_8UC1, Scalar(0));
-
     int count = 5000;
     int length = 10;
-    int thickness = 1;
+    int thickness = 0;
     float percent = 0.2;
     bool BW = true;
     for (int i = 0; i < count; ++i)
     {
+    	Mat visited = Mat(img.rows,img.cols, CV_8UC1, Scalar(0));
     	int u = rand() % img.rows;
     	int v = rand() % img.cols;
 
@@ -154,7 +160,7 @@ void sketching(const Mat & img)
 		                
 		                if(pos.x >= 0 && pos.x < img.cols && pos.y >= 0 && pos.y < img.rows)
                 		{
-                    		if(visited.at<unsigned char>(pos.x, pos.y) == 0)
+                    		if(visited.at<unsigned char>(pos) == 0)
                     		{
                     			Vec3b& pix = white.at<Vec3b>(pos);
                     			const Vec3b& temp = BW ? Vec3b(255, 255, 255) : img.at<Vec3b>(pos);
@@ -175,13 +181,210 @@ void sketching(const Mat & img)
 		}
     }
 
-	imshow("white", white);
-	imshow("gradX", gradX);
-	imshow("gradY", gradY);
-
-	cvWaitKey();
+	imwrite("sketchingLinesGradient.jpg", white);
+	cout << "------ Saving Sketching ------" << endl;
 }
 
+//=======================================================================================
+// Sketching Spline
+//=======================================================================================
+
+typedef vector<double> vec;
+
+struct SplineSet{
+    double a;
+    double b;
+    double c;
+    double d;
+    double x;
+};
+
+vector<SplineSet> spline(vec &x, vec &y)
+{
+    int n = x.size()-1;
+    vec a;
+    a.insert(a.begin(), y.begin(), y.end());
+    vec b(n);
+    vec d(n);
+    vec h;
+
+    for(int i = 0; i < n; ++i)
+        h.push_back(x[i+1]-x[i]);
+
+    vec alpha;
+    for(int i = 0; i < n; ++i)
+        alpha.push_back( 3*(a[i+1]-a[i])/h[i] - 3*(a[i]-a[i-1])/h[i-1]  );
+
+    vec c(n+1);
+    vec l(n+1);
+    vec mu(n+1);
+    vec z(n+1);
+    l[0] = 1;
+    mu[0] = 0;
+    z[0] = 0;
+
+    for(int i = 1; i < n; ++i)
+    {
+        l[i] = 2 *(x[i+1]-x[i-1])-h[i-1]*mu[i-1];
+        mu[i] = h[i]/l[i];
+        z[i] = (alpha[i]-h[i-1]*z[i-1])/l[i];
+    }
+
+    l[n] = 1;
+    z[n] = 0;
+    c[n] = 0;
+
+    for(int j = n-1; j >= 0; --j)
+    {
+        c[j] = z [j] - mu[j] * c[j+1];
+        b[j] = (a[j+1]-a[j])/h[j]-h[j]*(c[j+1]+2*c[j])/3;
+        d[j] = (c[j+1]-c[j])/3/h[j];
+    }
+
+    vector<SplineSet> output_set(n);
+    for(int i = 0; i < n; ++i)
+    {
+        output_set[i].a = a[i];
+        output_set[i].b = b[i];
+        output_set[i].c = c[i];
+        output_set[i].d = d[i];
+        output_set[i].x = x[i];
+    }
+    return output_set;
+}
+
+void sketchingSplines(const Mat & img)
+{
+
+	cout << "------ Sketching Splines ------" << endl;
+
+	Mat gradX, gradY;
+  	Mat gray, gray16;
+  	Mat white, overlay;
+
+  	toGray(img,gray);
+
+  	Sobel(gray, gradX, CV_16S, 0, 1, 3);
+    Sobel(gray, gradY, CV_16S, 1, 0, 3);
+
+    gradX = abs(gradX);
+    gradY = abs(gradY);
+
+    gradX.convertTo(gradX, CV_8U);
+    gradY.convertTo(gradY, CV_8U);
+
+    createWhiteImage(img.rows,img.cols,white);
+
+    int count = 10000;
+    int length = 10;
+    int thickness = 0;
+    float percent = 0.05;
+    bool BW = true;
+    int maxSize = 20;
+    int threshold = 150;
+
+    for (int i = 0; i < count; ++i)
+    {
+    	Mat visited = Mat(img.rows,img.cols, CV_8UC1, Scalar(0));
+    	vec X, Y;
+
+    	int u = rand() % img.rows;
+    	int v = rand() % img.cols;
+
+    	int valGradX = gradX.at<unsigned char>(u,v);
+    	int valGradY = gradY.at<unsigned char>(u,v);
+
+    	float norm = sqrt(valGradY*valGradY + valGradX* valGradX);
+
+    	if(norm > threshold)
+    	{
+    		X.push_back(v);
+    		Y.push_back(u);
+			
+    		for (int i = 0; i < maxSize; ++i)
+    		{
+    			u+=(-valGradY/norm)*length;
+    			v+=(valGradX/norm)*length;
+
+    			if(v >= 0 && v < img.cols && u >= 0 && u < img.rows)
+    			{
+    				valGradX = gradX.at<unsigned char>(u, v);
+    				valGradY = gradY.at<unsigned char>(u, v);
+
+		   			norm = sqrt(valGradY*valGradY + valGradX*valGradX);
+	    			if(norm > threshold)
+	    			{
+	    				X.push_back(v);
+	    				Y.push_back(u);
+	    			}
+	    			else break;
+    			}
+    			else break;
+    		}
+
+    		if(X.size() == 1)
+    		{
+    			
+    		}
+    		else
+    		{
+	    		vector<SplineSet> cs = spline(X, Y);
+			    for(unsigned int i = 0; i < cs.size(); ++i)
+			    { 
+			    	double tempX1, tempX2;
+			    	if(X[i] < X[i+1]) 
+			    	{
+			    		tempX1 = X[i];
+			    		tempX2 = X[i+1];
+			    	}
+			    	else
+			    	{
+			    		tempX2 = X[i];
+			    		tempX1 = X[i+1];
+			    	}
+
+			    	const Vec3b& temp = BW ? Vec3b(255, 255, 255) : img.at<Vec3b>(Point(X[0], Y[0]));
+
+			        for (double t = tempX1; t < tempX2; t+=0.05)
+			        {
+			        	for(int x = -thickness; x <= thickness; x++)
+					    {
+					        for(int  y= -thickness; y <= thickness; y++)
+					        {
+					           	double St = cs[i].a + cs[i].b * (t-X[i]) + cs[i].c * (t-X[i]) * (t-X[i]) + cs[i].d * (t-X[i]) * (t-X[i]) * (t-X[i]);
+					            Point pos = Point((int)t,(int)St) + Point(x,y);
+
+					            if(pos.x >= 0 && pos.x < img.cols && pos.y >= 0 && pos.y < img.rows)
+			               		{
+			                  		if(visited.at<unsigned char>(pos) == 0)
+			                   		{
+			                   			Vec3b& pix = white.at<Vec3b>(pos);
+			                   			
+		                    			if(BW) pix -= temp * percent;
+		                    			else pix = temp;
+
+		                    			visited.at<unsigned char>(pos) = (unsigned char)1;
+		                    		}
+		                    	}
+				    		}
+				    	}
+			        }
+			    }
+    		}
+		    
+		}
+		else
+		{
+			i--;
+		}
+	}
+
+	imwrite("sketchingSplinesGradient.jpg", white);
+	// imshow("white", white);
+	// imshow("GradX", gradX);
+	// imshow("GradY", gradY);
+	cout << "------ Saving Sketching ------" << endl;
+}
 
 //=======================================================================================
 //=======================================================================================
@@ -195,10 +398,6 @@ int main(int argc, char** argv){
   	}
 
   	Mat inputImageSrc;
-  	Mat inputImageSrcGray;
-  	Mat gradX, gradY;
-  	Mat white, overlay;
-
 
   	// Ouvrir l'image d'entr�e et v�rifier que l'ouverture du fichier se d�roule normalement
   	inputImageSrc = imread(argv[1], CV_LOAD_IMAGE_COLOR);
@@ -208,10 +407,10 @@ int main(int argc, char** argv){
         return -1;
     }
 
-    toGray(inputImageSrc, inputImageSrcGray);
+    sketchingSplines(inputImageSrc);
 
-    //sketchingGray(inputImageSrcGray);
-    sketching(inputImageSrc);
+    sketchingLines(inputImageSrc);
 
+	cvWaitKey();
   	return 0;
 }
